f4c4b0b60f46c8dcee44f02258e3066c62b9bfee
harmolipi <nikob381@gmail.com>
2021-02-22 13:59:59 -0500

    Finished bubble sort project

---
+++ bubble_sort.rb
@@ -0,0 +1,17 @@
+def bubble_sort(array)
+    sorted = false
+
+    until sorted
+        sorted = true
+        for i in 0...(array.length - 1)
+            if array[i] > array[i+1]
+                array[i], array[i+1] = array[i+1], array[i]
+                sorted = false
+            end
+        end
+    end
+
+    array
+end
+
+p bubble_sort([1, 3, 12, 105, 4, -5, 2, -26, 6, 2, 4])
